848d8ba6d50f1b7255e637022f4afef5d94a7fa8
Santiago Alvarez <santiiairel2004@gmail.com>
2026-03-20 18:48:40 -0300

    se agrego la funcionalidad de multiples juegos aplicando sample()

--- questions.py
+++ questions.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-'''
 words = [
     "python",
     "programa",
@@ -12,67 +11,58 @@ words = [
     "entero",
     "lista",
 ]
-'''
 
-categorias = {
-    "conceptos": ["programa", "variable", "funcion", "bucle"],
-    "datos": ["cadena", "entero", "lista"],
-    "lenguajes": ["python"]
-}
 
 letters = string.ascii_lowercase
-# word = random.choice(words)
-guessed = []
-attempts = 6
+# devuelve una lista completa de palabras en orden aleatorio.
+words = random.sample(words, len(words))
 
 print("¡Bienvenido al Ahorcado!")
 print()
-print('ingrese una de las siguentes categorias')
-print(f'{categorias.keys()}')
-categoria = input('ingrese el nombre de una categoria si lo desea sino ingrese -1')
-if categoria != '-1':
-    word = random.choice(categorias[categoria])
-else:
-    lista_palabras = random.choice(list(categorias.values()))
-    word = random.choice(lista_palabras)
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
+for word in words:
+    attempts = 6
+    guessed = []
+
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+
+        print(progress)
+
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print(f"¡Ganaste! Obtuviste un total de {attempts} puntos")
+            break
+
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+
+        letter = input("Ingresá una letra: ").lower()
+
+        # Si no es una letra o si posee mas de un caracter se considera invalido
+        if ( (not letter in letters) or (len(letter) > 1)):
+            print('Entrada no válida')
+            continue
+
         if letter in guessed:
-            progress += letter + " "
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
         else:
-            progress += "_ "
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
 
-    print(progress)
+        print()
 
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print(f"¡Ganaste! Obtuviste un total de {attempts} puntos")
-        break
-
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-
-    letter = input("Ingresá una letra: ").lower()
-
-    # Si no es una letra o si posee mas de un caracter se considera invalido
-    if ( (not letter in letters) or (len(letter) > 1)):
-        print('Entrada no válida')
-        continue
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
+        print(f"¡Perdiste! La palabra era: {word} obtuviste un total de {attempts}")
 
-    print()
-
-else:
-    print(f"¡Perdiste! La palabra era: {word} obtuviste un total de {attempts}")
+print("¡Se acabaron todas las palabras! Gracias por jugar.")
